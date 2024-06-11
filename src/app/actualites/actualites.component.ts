c4ecb693db0d2fe4bae85d3a4c706e837d51d8c8
ahmedfourati11 <ahmedfourati44@gmail.com>
2024-06-11 15:13:10 +0200

    updates

--- src/app/actualites/actualites.component.ts
+++ src/app/actualites/actualites.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,OnInit,AfterViewInit,ElementRef,Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-actualites',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './actualites.component.html',
   styleUrl: './actualites.component.css'
 })
-export class ActualitesComponent {
+export class ActualitesComponent implements AfterViewInit {
+
+  constructor(private elRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    const slides = this.elRef.nativeElement.querySelectorAll('.carousel-item');
+    let currentIndex = 0;
+
+    const showSlide = (index: number) => {
+      slides.forEach((slide: HTMLElement, i: number) => {
+        if (i === index) {
+          slide.classList.add('active');
+        } else {
+          slide.classList.remove('active');
+        }
+      });
+    };
+
+    const nextSlide = () => {
+      currentIndex = (currentIndex + 1) % slides.length;
+      showSlide(currentIndex);
+    };
+
+    setInterval(nextSlide, 6000);
+
+    showSlide(currentIndex);
+  }
 
 }
